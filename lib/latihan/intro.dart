af4decfc7c41e0fe77d15d5ecce1b8f722727479
KRISMI <krismi@KRISMIs-MacBook-Air.local>
2026-06-23 11:44:09 +0700

    first commit

---
+++ lib/latihan/intro.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Intro Page Inixindo'),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+      body: const Center(
+        child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Icon(Icons.flutter_dash, size: 100, color: Colors.blue),
+              SizedBox(height: 20),
+              Text(
+                'Selamat Datang di Latihan Flutter!',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 10),
+              Text(
+                'Ini adalah halaman intro.dart yang baru dibuat.',
+                style: TextStyle(fontSize: 16, color: Colors.grey),
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
